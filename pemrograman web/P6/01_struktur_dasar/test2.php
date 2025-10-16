df0d8883dbc6fa4d1b946465160ac0bc81542ee9
AbiZakaRiaSunandar <2411102441030@umkt.ac.id>
2025-10-16 13:14:28 +0800

    Add files via upload

---
+++ pemrograman web/P6/01_struktur_dasar/test2.php
@@ -0,0 +1,6 @@
+<?php
+eco "Hello World"; # kesalahan pada nama eco seharusnya di ubah menjadi (echo) agar code berjalan
+echo "Hello World";
+echo "Hello World <br />";
+echo "Hello World <br />";
+?>
